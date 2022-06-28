d4916705a2c3cbfed2d73ab33e01d8de3440fc91
agouy <contact@agouy.com>
2022-06-28 12:58:37 +0000

    feat: script to check test status

---
+++ check_test_status.R
@@ -0,0 +1,79 @@
+library(jsonlite)
+library(gh)
+library(dplyr)
+
+## read library report
+report_url <- "https://raw.githubusercontent.com/tercen/app-library/master/library-report.json"
+library_report <- jsonlite::fromJSON(report_url)
+
+## Failed test operators
+for(op_id in seq_len(nrow(library_report$failed_operators))) {
+  op <- library_report$failed_operators[op_id, ]
+  issue_hash <- digest::digest(op, algo = "murmur32")
+  op_name <- basename(op$url)
+  
+  ### Check existing GH Issue
+  existing_issues <- gh(
+    "GET /repos/{owner}/{repo}/issues",
+    owner = 'tercen',
+    repo = 'app-library',
+    .accept = 'application/vnd.github.v3+json',
+    .token = Sys.getenv("GITHUB_TOKEN")
+  )
+  issue_titles <- unlist(lapply(existing_issues, "[[", "title"))
+  
+  ### Create new issue if different hash
+  if(!any(grepl(issue_hash, issue_titles))) {
+    info <- paste0("\nVersion: ", op$version, "\n\n```\n",op$error,"\n```\n")
+    title_failed <- paste0("Failed test: ", op_name, " [", issue_hash, "]")
+    body_failed <- paste0("The unit test associated with this operator has failed. If you would like this operator to be part of the App Library, the test must be fixed.\n",info,"\n\nNote: This issue has automatically been created by Tercen's operator sentinel.")
+
+    gh(
+      "POST /repos/{owner}/{repo}/issues",
+      owner = 'tercen',
+      repo = 'app-library',
+      title = title_failed,
+      body = body_failed,
+      milestone = '2',
+      labels = list("enhancement", "compliance"),
+      .token = Sys.getenv("GITHUB_TOKEN")
+    )
+  }
+  
+}
+
+## No test and no release operators
+for(op in c(library_report$no_release_operators, library_report$notest_operators$url)) {
+  issue_hash <- digest::digest(op, algo = "murmur32")
+  op_name <- basename(op)
+  
+  ### Check existing GH Issue
+  existing_issues <- gh(
+    "GET /repos/{owner}/{repo}/issues",
+    owner = 'tercen',
+    repo = 'app-library',
+    .accept = 'application/vnd.github.v3+json',
+    .token = Sys.getenv("GITHUB_TOKEN")
+  )
+  issue_titles <- unlist(lapply(existing_issues, "[[", "title"))
+
+  ### Create new issue if different hash
+  if(!any(grepl(issue_hash, issue_titles))) {
+    
+    title_norelease <- paste0("Add a unit test to ", op_name, " [", issue_hash, "]")
+    body_norelease <- paste0("There is no unit test associated with this operator (",op,"). If you would like this operator to be part of the App Library, it must include [continuous integration workflows](https://tercen.github.io/developers_guide/continuous-integration.html).\n\nNote: This issue has automatically been created by Tercen's operator sentinel.")
+  
+    gh(
+      "POST /repos/{owner}/{repo}/issues",
+      owner = 'tercen',
+      repo = 'app-library',
+      title = title_norelease,
+      body = body_norelease,
+      milestone = '1',
+      labels = list("enhancement", "compliance"),
+      .token = Sys.getenv("GITHUB_TOKEN")
+    )
+  }
+
+}
+
